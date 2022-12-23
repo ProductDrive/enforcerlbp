c183ec7a4cf6d2e7013329dfe42340557302ae3c
afeexclusive <afeexclusive@gmail.com>
2022-12-23 17:29:33 +0100

    exercise prescription logic with DTO

---
+++ enforcerlbp/DTOs/RequestObject/ExercisePrescriptionDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTOs.RequestObject
+{
+    public class ExercisePrescriptionDTO
+    {
+
+        public Guid ID { get; set; }
+        public Guid ExerciseId { get; set; }
+        public string ExerciseName { get; set; }
+        public Guid PatientId { get; set; }
+        public Guid PhysiotherapistId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Repetitions { get; set; }
+        public string Set { get; set; }
+        public string Hold { get; set; }
+        public string Time { get; set; }
+        public string Description { get; set; }
+        public string ExerciseSummary { get; set; }
+        public string SubmittedVideoUrl { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+        public ExercisePrescriptionDTO()
+        {
+            ExerciseSummary = $"{ExerciseName}-{Repetitions}-{Time} to be completed {EndDate.ToString("d")}";
+        }
+    }
+}
